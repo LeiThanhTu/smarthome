abed4d7c7cfd1b18dde96304937d83ef56816f58
LeiThanhTu <lethithanhtu18@gmail.com>
2025-09-16 17:33:49 +0700

    update 16-09-2025/ok

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth.store";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_URL,
